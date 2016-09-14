e543153211773b0f6be15c3f8b5abf6e21b4a92f
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-09-14 15:09:11 +0800

    clear gem

--- lib/ruby/skyline/core.rb
+++
@@ -1,9 +0,0 @@
-require "ruby/skyline/core/version"
-
-module Ruby
-  module Skyline
-    module Core
-      # Your code goes here...
-    end
-  end
-end
--- lib/ruby/skyline/core/version.rb
+++
@@ -1,7 +0,0 @@
-module Ruby
-  module Skyline
-    module Core
-      VERSION = "0.1.0"
-    end
-  end
-end
