cb33024c54565d18a902b48b7e68e95ecf89f052
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-09-14 15:53:52 +0800

    fix version

--- spec/ruby-skyline-core_spec.rb
+++ spec/ruby-skyline-core_spec.rb
@@ -5,7 +5,7 @@ require_relative 'spec_helper'
 
 describe RubySkylineCore do
   it 'has a version number' do
-    expect(Ruby::Skyline::Core::VERSION).not_to be nil
+    expect(RubySkylineCore::VERSION).not_to be nil
   end
 end
 
