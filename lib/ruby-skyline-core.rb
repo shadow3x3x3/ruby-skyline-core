37f4bc946d0a7684dc4a65da402f0f0452f0054e
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-09-17 14:29:40 +0800

    better error control

--- lib/ruby-skyline-core.rb
+++ lib/ruby-skyline-core.rb
@@ -2,8 +2,7 @@
 class Array
   # dominate function in skyline for array
   def dominate?(array)
-    raise ArgumentError, "Need Array not #{array.class}"    unless array.class == Array
-    raise ArgumentError, 'Two Arrays are not the same size' unless size == array.size
+    pre_check_skyline_domiante(array)
     flag       = 0
     check_flag = size
     each_with_index do |attr, index|
@@ -18,12 +17,24 @@ class Array
 
   # skyline attributes aggregate for array
   def aggregate(array)
+    pre_check_skyline_domiante(array)
     aggregate_array = []
     raise ArgumentError, "Need Array not #{array.class}"    unless array.class == Array
-    raise ArgumentError, 'Two Arrays are not the same size' unless size == array.size
     each_with_index do |attr, index|
       aggregate_array << (attr + array[index]).round(6)
     end
     aggregate_array
+  end
+
+  # Skyline Error Handling
+  def pre_check_skyline_domiante(target_array)
+    unless target_array.class == Array
+      raise ArgumentError, "excpet (Array) got (#{target_array.class})" 
+    end
+
+    unless size == target_array.size
+      raise ArgumentError,
+        "size dosen't match, excpet (#{size}) got (#{target_array.size})"
+    end
   end
 end
